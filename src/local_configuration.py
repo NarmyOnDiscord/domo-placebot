e47c87c513972d2444afbce1018ee6eed39a2479
Narmy <jo25bu@gmail.com>
2022-04-04 17:22:59 +0200

    Custom Config beim Start angeben

--- src/local_configuration.py
+++ src/local_configuration.py
@@ -3,5 +3,7 @@ import json
 # Do I look like I know python? :)
 local_configuration = {}
 
-with open('config.json', 'r') as f:
+x = input("Bitte gebe den Namen der Config mit den Accounts und der Target CU an: ")
+
+with open(f'/home/placebot/{x}.json', 'r') as f:
     local_configuration = json.load(f)
